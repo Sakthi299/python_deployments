d921d5b49b73392d7f1add0ef66033df21c3b70d
Sakthi Namasivayam <60822530+Sakthi299@users.noreply.github.com>
2020-09-10 17:15:31 +0530

    python_webframework
    creating webframework in python using flask and connecting the backend of webapplication with sqlite3 db for data collection

--- Local_password_manager/app.py
+++ Local_password_manager/app.py
@@ -1,25 +1,64 @@
-from flask import Flask,redirect,render_template,request,url_for
+from flask import Flask,redirect,render_template,url_for,request
 import sqlite3
-app=Flask(__name__)
-@app.route('/')
-def index():
-    return render_template("practice.html")
+app = Flask(__name__)
+
+@app.route("/",methods=["POST","GET"])
+def login():
+  return render_template("indexpage.html")
+
+@app.route("/nextform",methods=["POST","GET"])
+def nextform():
+  if request.method == 'POST':
+    return render_template("index.html")
+
+@app.route("/listform",methods=["POST","GET"])
+def listform():
+  if request.method == 'POST':
+    return render_template("entry.html")
+
+@app.route("/det",methods=["POST","GET"])
+def save():
+   msg="msg"
+   if request.method == "POST":
+     nam=request.form["name"]
+     pas=request.form["pass"]
+     lin=request.form["link"]
+     do=request.form["dom"]
+     n=str(nam).lower()
+     l=str(lin).lower()
+     d=str(do).lower()
+     with sqlite3.connect("C:\\Users\\HP\\Documents\\PythonWorks\\data.db") as con:
+       msg="connected"
+       try:
+        cur=con.cursor()
+        msg="good"
+        cur.execute("INSERT INTO login VALUES('"+d+"', '"+n+"', '"+pas+"', '"+l+"')")
+        con.commit(
+        )
+        msg="added successfully"
+       except:
+        con.rollback()
+       finally:
+        return render_template("index.html")
+        con.close()
 
 @app.route('/view',methods=["POST","GET"])
 def view():
-    if request.method=="POST":
-        key=request.form["id"]
+    if request.method == "POST":
+        value=request.form["id"]
+        key=str(value).lower()
         try:
-            conn=sqlite3.connect('C:\\Users\\HP\\Documents\\PythonWorks\\sqlite-tools-win32-x86-3330000\\manager.db')
+            conn=sqlite3.connect('C:\\Users\\HP\\Documents\\PythonWorks\\data.db')
             with conn:
-                conn.row_factory=sqlite3.Row
+                conn.row_factory = sqlite3.Row
                 cur=conn.cursor()
-                cur.execute('''select * from wallet where domain=? OR name=? OR weblink=?''',(key,key,key))
+                cur.execute('''select * from login where domain=? OR name=? OR weblink=?''',(key,key,key))
                 rrr=cur.fetchall()
         except Exception as e:
             print("error: "+str(e))
-        finally:
-            return render_template( "practice.html",rows=rrr)
 
-if __name__=="main":
-    app.run(debug=True)
+        finally:
+            return render_template("finish.html",rows = rrr)
+
+if __name__ == "main":
+ app.run(debug=True)
